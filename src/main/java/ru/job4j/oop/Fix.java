e101a69a1b85f77dc6ff207fbed7cbc50fc381f3
games-pc <denis2maksimov@gmail.com>
2020-12-19 14:08:29 +0300

    1.1.1. Конструктор по умолчанию [#394969]

---
+++ src/main/java/ru/job4j/oop/Fix.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+
+public class Fix {
+    private String desc;
+
+    public Fix(String text) {
+        desc = text;
+    }
+
+    public static void main(String[] args) {
+        String issue = "Ok, Google!";
+        Fix item = new Fix(issue);
+    }
+}
